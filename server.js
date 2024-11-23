faeeeb7ec79c6596c5621984b198e25088c8455f
Roni Twito <ronitwito08@gmail.com>
2024-11-23 14:54:21 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+
+
+// MongoDB Connection
+mongoose.connect('mongodb://localhost:27017/WebApplications_A1', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB successfully!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Failed to connect to MongoDB:', err);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
